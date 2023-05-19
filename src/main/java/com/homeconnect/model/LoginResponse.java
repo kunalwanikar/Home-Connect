6b7e14f8e4ffc7589d856989a73417f7d474932f
Kunal Wanikar <kunal.wanikar@olacabs.com>
2023-05-19 12:34:23 +0530

    Added login request response model

---
+++ src/main/java/com/homeconnect/model/LoginResponse.java
@@ -0,0 +1,52 @@
+package com.homeconnect.model;
+
+public class LoginResponse {
+    private boolean status;
+    private String message;
+    private String loginToken;
+
+    public String getLoginToken() {
+        return loginToken;
+    }
+
+    public void setLoginToken(String loginToken) {
+        this.loginToken = loginToken;
+    }
+
+    private long userId;
+
+    private int userType;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
